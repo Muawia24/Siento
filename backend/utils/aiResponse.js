78ae8055459ffd3c8448349574c56011e7335115
Muawia24 <amuawia666@gmail.com>
2025-03-06 20:51:26 +0200

    Changed Hugging face model

--- backend/utils/aiResponse.js
+++ backend/utils/aiResponse.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-//require('dotenv').config();
 
 const generateResponse = async (text) => {
     try {
         const response = await axios.post(
-            'https://api-inference.huggingface.co/models/openassistant/oasst-sft-4-pythia-12b-epoch-3.5',
+            'https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill',
             { inputs: `Analyze this mood entry and give a supportive response: ${text}` },
             { headers: { Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}` } }
         );
-        return response.data.generated_text || "I appreciate you sharing. Stay strong!";
+        console.log(response.data);
+        return response.data[0]. generated_text //|| "I appreciate you sharing. Stay strong!";
     } catch (error) {
         console.error("Error generating AI response:", error);
         return "I appreciate you sharing. Stay strong!";
